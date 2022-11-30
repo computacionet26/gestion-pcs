cbae4ac7c1e4a8b7ea132ada4a87a9e4976fc3eb
LucianoBrumer <lucianobrumer5@gmail.com>
2022-11-30 09:53:25 -0300

    env example

--- src/app.js
+++ src/app.js
@@ -10,10 +10,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use('/user', require('./src/routes/user.route'))
-app.use('/role', require('./src/routes/role.route'))
-app.use('/lab', require('./src/routes/lab.route'))
-app.use('/device', require('./src/routes/device.route'))
+app.use('/user', require('./routes/user.route'))
+app.use('/role', require('./routes/role.route'))
+app.use('/lab', require('./routes/lab.route'))
+app.use('/device', require('./routes/device.route'))
 
 app.listen(port, () => {
     console.log(`Server on http://localhost:${port}`);
